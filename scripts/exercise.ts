57ff9dbee65e09c3210a9213b3e29e9dae85c17c
amifed <amifed.me@gmail.com>
2022-10-16 13:05:51 +0800

    feat: update exercise file generator script

--- scripts/exercise.ts
+++ scripts/exercise.ts
@@ -1,4 +1,4 @@
-import { execSync } from 'child_process'
+import { exec, execSync } from 'child_process'
 import path from 'path'
 import fg from 'fast-glob'
 import fs from 'fs-extra'
@@ -12,6 +12,7 @@ import { toLinks } from './actions/utils/toLinks'
 import type { SupportedLocale } from './locales'
 import { defaultLocale, t } from './locales'
 import type { Quiz } from './types'
+import { formatToCode } from './actions/utils/formatToCode'
 
 const [, , no] = process.argv
 
@@ -31,25 +32,16 @@ if (!folders.length) {
   console.log('Please specify an valid questions number')
   process.exit(1)
 }
+const exerciseFile = path.resolve(QUIZ_ROOT, folders[0], 'exercise.ts')
 
-const formatToCode = (quiz: Partial<Quiz>, locale: SupportedLocale) => {
-  return `${toDivider(t(locale, 'divider.code-start'))
-  }\n${
-    (quiz.template || '').trim()
-  }\n\n${
-    toDivider(t(locale, 'divider.test-cases'))
-  }${quiz.tests || ''
-  }\n\n`
+const generator = async () => {
+  const quiz = await loadQuizByNo(padNo)
+  if (!fs.existsSync(exerciseFile) && quiz)
+    await fs.writeFile(exerciseFile, formatToCode(quiz, 'zh-CN'))
+  exec(`eslint ${exerciseFile} --fix`)
 }
-const exerciseDir = path.resolve(QUIZ_ROOT, folders[0])
-const template = fs.readFileSync(path.join(exerciseDir, 'template.ts'), 'utf-8')
-const tests = fs.readFileSync(path.join(exerciseDir, 'test-cases.ts'), 'utf-8') || ''
-const exerciseFile = path.resolve(exerciseDir, 'exercise.ts')
 
-if (!fs.existsSync(exerciseFile))
-  fs.writeFileSync(exerciseFile, formatToCode({ template, tests }, 'zh-CN'))
-
-console.log(exerciseFile)
+generator()
 
 chokidar.watch(exerciseFile).on('all', () => {
   try {
