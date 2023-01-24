bbccba2605bf5dbddc550d54fbe46dc9e5dd6cf9
amifed <amifed.me@gmail.com>
2023-01-24 23:57:39 +0800

    feat: exercise medium

---
+++ questions/00645-medium-diff/exercise.ts
@@ -0,0 +1,71 @@
+/*
+  645 - Diff
+  -------
+  by ZYSzys (@ZYSzys) #中等 #object
+
+  ### 题目
+
+  获取两个接口类型中的差值属性。
+
+  ```ts
+  type Foo = {
+    a: string;
+    b: number;
+  }
+  type Bar = {
+    a: string;
+    c: boolean
+  }
+
+  type Result1 = Diff<Foo,Bar> // { b: number, c: boolean }
+  type Result2 = Diff<Bar,Foo> // { b: number, c: boolean }
+
+  ```
+
+  > 在 Github 上查看：https://tsch.js.org/645/zh-CN
+*/
+
+/* _____________ 你的代码 _____________ */
+
+// Solution #1
+// type Diff<U extends Record<PropertyKey, unknown>, T extends Record<PropertyKey, unknown>> = {
+//   [K in keyof Omit<T, keyof U> | keyof Omit<U, keyof T>]: (Omit<T, keyof U> & Omit<U, keyof T>)[K];
+// }
+// Solution #2
+// type Diff<T, U> = {
+//   [K in Exclude<keyof (T & U), keyof (T | U)>]: (T & U)[K];
+// }
+// Solution #3
+type Diff<T, U > = Omit<T & U, keyof T & keyof U>
+
+/* _____________ 测试用例 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type Foo = {
+  name: string
+  age: string
+}
+type Bar = {
+  name: string
+  age: string
+  gender: number
+}
+type Coo = {
+  name: string
+  gender: number
+}
+
+type cases = [
+  Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
+  Expect<Equal<Diff<Bar, Foo>, { gender: number }>>,
+  Expect<Equal<Diff<Foo, Coo>, { age: string; gender: number }>>,
+  Expect<Equal<Diff<Coo, Foo>, { age: string; gender: number }>>,
+]
+
+/* _____________ 下一步 _____________ */
+/*
+  > 分享你的解答：https://tsch.js.org/645/answer/zh-CN
+  > 查看解答：https://tsch.js.org/645/solutions
+  > 更多题目：https://tsch.js.org/zh-CN
+*/
+
